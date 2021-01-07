454b55647af8ce87c471b1dd0cbf92c0d7fdf12c
ledoctah <matheusferr2015@gmail.com>
2021-01-07 17:33:54 -0300

    Fixed ESLint changes

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -8,45 +8,45 @@ import AppError from '@shared/errors/AppError';
 import User from '../infra/typeorm/entities/User';
 
 interface Request {
-	email: string;
+  email: string;
 
-	password: string;
+  password: string;
 }
 
 interface Response {
-	user: User;
+  user: User;
 
-	token: string;
+  token: string;
 }
 
 class AuthenticateUserService {
-	public async execute({ email, password }: Request): Promise<Response> {
-		const usersRepository = getRepository(User);
+  public async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
 
-		const user = await usersRepository.findOne({ where: { email }});
+    const user = await usersRepository.findOne({ where: { email } });
 
-		if(!user) {
-			throw new AppError('Incorrect email/password combination', 401);
-		}
+    if (!user) {
+      throw new AppError('Incorrect email/password combination', 401);
+    }
 
-		const passwordMatched = await compare(password, user.password);
+    const passwordMatched = await compare(password, user.password);
 
-		if(!passwordMatched) {
-			throw new AppError('Incorrect email/password combination', 401);
-		}
+    if (!passwordMatched) {
+      throw new AppError('Incorrect email/password combination', 401);
+    }
 
-		const { secret, expiresIn } = authConfig.jwt;
+    const { secret, expiresIn } = authConfig.jwt;
 
-		const token = sign({}, secret, {
-			subject: user.id,
-			expiresIn,
-		});
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn,
+    });
 
-		return {
-			user,
-			token
-		}
-	}
+    return {
+      user,
+      token,
+    };
+  }
 }
 
 export default AuthenticateUserService;
--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -8,36 +8,36 @@ import AppError from '@shared/errors/AppError';
 import User from '../infra/typeorm/entities/User';
 
 interface Request {
-	user_id: string;
+  user_id: string;
 
-	avatarFilename: string;
+  avatarFilename: string;
 }
 
 class updateUserAvatarService {
-	public async execute({ user_id, avatarFilename }: Request): Promise<User> {
-		const usersRepository = getRepository(User);
+  public async execute({ user_id, avatarFilename }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
 
-		const user = await usersRepository.findOne(user_id);
+    const user = await usersRepository.findOne(user_id);
 
-		if(!user) {
-			throw new AppError('Only authenticated users can change avatar.', 401);
-		}
+    if (!user) {
+      throw new AppError('Only authenticated users can change avatar.', 401);
+    }
 
-		if(user.avatar) {
-			const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-			const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
 
-			if(userAvatarFileExists) {
-				await fs.promises.unlink(userAvatarFilePath);
-			}
-		}
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
 
-		user.avatar = avatarFilename;
+    user.avatar = avatarFilename;
 
-		await usersRepository.save(user);
+    await usersRepository.save(user);
 
-		return user;
-	}
+    return user;
+  }
 }
 
 export default updateUserAvatarService;
