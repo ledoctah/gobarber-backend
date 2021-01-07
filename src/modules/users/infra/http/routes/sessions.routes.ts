656c6c42489064861b2ad3a5211343a1e9756847
ledoctah <matheusferr2015@gmail.com>
2021-01-07 17:57:56 -0300

    Fixed ESLint save changes

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -5,18 +5,18 @@ import AuthenticateUserService from '@modules/users/services/AuthenticateUserSer
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
-	const { email, password } = request.body;
+  const { email, password } = request.body;
 
-	const authenticateUserService = new AuthenticateUserService();
+  const authenticateUserService = new AuthenticateUserService();
 
-	const { user, token } = await authenticateUserService.execute({
-		email,
-		password,
-	});
+  const { user, token } = await authenticateUserService.execute({
+    email,
+    password,
+  });
 
-	delete user.password;
+  delete user.password;
 
-	return response.json({ user, token });
+  return response.json({ user, token });
 });
 
 export default sessionsRouter;
--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -9,36 +9,40 @@ import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarSer
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
-
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
-	const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-	const createUser = new CreateUserService();
+  const createUser = new CreateUserService();
 
-	const user = await createUser.execute({
-		name,
-		email,
-		password
-	});
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
 
-	delete user.password;
+  delete user.password;
 
-	return response.json(user);
+  return response.json(user);
 });
 
-usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
-	const updateUserAvatar = new UpdateUserAvatarService();
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    const updateUserAvatar = new UpdateUserAvatarService();
 
-	const user = await updateUserAvatar.execute({
-		user_id: request.user.id,
-		avatarFilename: request.file.filename,
-	});
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
 
-	delete user.password;
+    delete user.password;
 
-	return response.json(user);
-});
+    return response.json(user);
+  },
+);
 
 export default usersRouter;
